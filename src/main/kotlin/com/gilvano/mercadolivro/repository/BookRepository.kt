42d8e58d2ff27b04902473222f6835177a48d8ec
gilvanodb1 <94841963+gilvanodb1@users.noreply.github.com>
2021-12-15 16:23:48 -0300

    Criando outros métodos para book

--- src/main/kotlin/com/gilvano/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/gilvano/mercadolivro/repository/BookRepository.kt
@@ -1,8 +1,10 @@
 package com.gilvano.mercadolivro.repository
 
+import com.gilvano.mercadolivro.enums.BookStatus
 import com.gilvano.mercadolivro.model.BookModel
 import org.springframework.data.repository.CrudRepository
 
 interface BookRepository : CrudRepository<BookModel, Int> {
+    fun findByStatus(status: BookStatus): List<BookModel>
 
 }
