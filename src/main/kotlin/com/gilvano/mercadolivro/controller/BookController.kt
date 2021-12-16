2e43bda8ecb397273a89b09f49c2f7053f521b9c
gilvano <g.piontkoski@gmail.com>
2021-12-16 08:47:13 -0300

    Criando outros métodos para book

--- src/main/kotlin/com/gilvano/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/gilvano/mercadolivro/controller/BookController.kt
@@ -1,6 +1,7 @@
 package com.gilvano.mercadolivro.controller
 
 import com.gilvano.mercadolivro.controller.request.PostBookRequest
+import com.gilvano.mercadolivro.controller.request.PutBookRequest
 import com.gilvano.mercadolivro.extension.toBookModel
 import com.gilvano.mercadolivro.model.BookModel
 import com.gilvano.mercadolivro.service.BookService
@@ -20,6 +21,34 @@ class BookController(
     fun create(@RequestBody request: PostBookRequest) {
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> {
+        return bookService.findAll()
+    }
+
+    @GetMapping("/actives")
+    fun findActives(): List<BookModel> {
+        return bookService.findActives()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody request: PutBookRequest) {
+        val bookSaved = bookService.findById(id)
+        bookService.update(request.toBookModel(bookSaved))
     }
 
 }
---
+++ src/main/kotlin/com/gilvano/mercadolivro/controller/request/PutBookRequest.kt
@@ -0,0 +1,8 @@
+package com.gilvano.mercadolivro.controller.request
+
+import java.math.BigDecimal
+
+data class PutBookRequest (
+    var name: String? = null,
+    var price: BigDecimal
+)
