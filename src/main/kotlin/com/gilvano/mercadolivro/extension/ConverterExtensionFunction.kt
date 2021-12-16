ea13c21796a0041e9b7ff2188da4923e1149f671
gilvano <g.piontkoski@gmail.com>
2021-12-16 08:33:43 -0300

    Revert "Implementar post para book"
    This reverts commit 0032f607e8690e76055cd43d1104be01cf51e05a.

--- src/main/kotlin/com/gilvano/mercadolivro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/gilvano/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -1,10 +1,7 @@
 package com.gilvano.mercadolivro.extension
 
-import com.gilvano.mercadolivro.controller.request.PostBookRequest
 import com.gilvano.mercadolivro.controller.request.PostCustomerRequest
 import com.gilvano.mercadolivro.controller.request.PutCustomerRequest
-import com.gilvano.mercadolivro.enums.BookStatus
-import com.gilvano.mercadolivro.model.BookModel
 import com.gilvano.mercadolivro.model.CustomerModel
 
 fun PostCustomerRequest.toCustomModel(): CustomerModel {
@@ -13,13 +10,4 @@ fun PostCustomerRequest.toCustomModel(): CustomerModel {
 
 fun PutCustomerRequest.toCustomModel(id: Int): CustomerModel {
     return CustomerModel(id = id, name = this.name, email = this.email)
-}
-
-fun PostBookRequest.toBookModel(customer: CustomerModel?): BookModel {
-    return BookModel(
-        name = this.name,
-        price = this.price,
-        status = BookStatus.ATIVO,
-        customer = customer
-    )
 }
--- src/main/kotlin/com/gilvano/mercadolivro/repository/BookRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.gilvano.mercadolivro.repository
-
-import com.gilvano.mercadolivro.model.BookModel
-import org.springframework.data.repository.CrudRepository
-
-interface BookRepository : CrudRepository<BookModel, Int> {
-
-}
