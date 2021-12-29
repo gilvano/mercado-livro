627200cede69198d836dca38304e1d6d7495c1bb
gilvano <g.piontkoski@gmail.com>
2021-12-29 16:19:56 -0300

    Encrypt password

--- src/main/kotlin/com/gilvano/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/gilvano/mercadolivro/service/CustomerService.kt
@@ -6,12 +6,14 @@ import com.gilvano.mercadolivro.enums.Profile
 import com.gilvano.mercadolivro.exception.NotFoundException
 import com.gilvano.mercadolivro.model.CustomerModel
 import com.gilvano.mercadolivro.repository.CustomerRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService(
-    val customerRepository: CustomerRepository,
-    val bookService: BookService
+    private val customerRepository: CustomerRepository,
+    private val bookService: BookService,
+    private val bCrypt: BCryptPasswordEncoder
 ) {
     fun getAll(name: String?): List<CustomerModel> {
         name?.let {
@@ -22,7 +24,8 @@ class CustomerService(
 
     fun create(customer: CustomerModel): CustomerModel {
         val customerCopy = customer.copy(
-            roles = setOf(Profile.CUSTOMER)
+            roles = setOf(Profile.CUSTOMER),
+            password = bCrypt.encode(customer.password),
         )
         return customerRepository.save(customerCopy)
     }
