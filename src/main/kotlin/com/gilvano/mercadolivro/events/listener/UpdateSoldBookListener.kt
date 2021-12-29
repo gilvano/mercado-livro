220ab099630aca0a35278abbbaf60271396f474c
gilvano <g.piontkoski@gmail.com>
2021-12-29 15:05:46 -0300

    Change event listeners to async

--- src/main/kotlin/com/gilvano/mercadolivro/events/listener/UpdateSoldBookListener.kt
+++ src/main/kotlin/com/gilvano/mercadolivro/events/listener/UpdateSoldBookListener.kt
@@ -2,16 +2,16 @@ package com.gilvano.mercadolivro.events.listener
 
 import com.gilvano.mercadolivro.events.PurchaseEvent
 import com.gilvano.mercadolivro.service.BookService
-import com.gilvano.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class UpdateSoldBookListener(
     private val bookService: BookService
 ) {
 
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
         bookService.purchase(purchaseEvent.purchaseModel.books)
