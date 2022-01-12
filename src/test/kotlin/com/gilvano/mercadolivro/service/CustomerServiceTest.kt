39fe4358dde258e7bc87240f95d9a0528dba413a
gilvano <g.piontkoski@gmail.com>
2022-01-12 10:33:58 -0300

    Create customer service test

---
+++ src/test/kotlin/com/gilvano/mercadolivro/service/CustomerServiceTest.kt
@@ -0,0 +1,62 @@
+package com.gilvano.mercadolivro.service
+
+import com.gilvano.mercadolivro.enums.CustomerStatus
+import com.gilvano.mercadolivro.enums.Role
+import com.gilvano.mercadolivro.model.CustomerModel
+import com.gilvano.mercadolivro.repository.CustomerRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+
+    @MockK
+    private lateinit var customerRepository: CustomerRepository
+
+    @MockK
+    private lateinit var bookService: BookService
+
+    @MockK
+    private lateinit var bCrypt: BCryptPasswordEncoder
+
+
+    @InjectMockKs
+    private lateinit var customerService: CustomerService
+
+    @Test
+    fun `should return all customers`() {
+        val fakeCustomers = listOf(buildCustomer(), buildCustomer())
+
+        every { customerRepository.findAll() } returns fakeCustomers
+
+        val customers = customerService.getAll(null)
+
+        assertEquals(fakeCustomers, customers)
+        verify(exactly = 1) { customerRepository.findAll() }
+        verify(exactly = 0) { customerRepository.findByNameContaining(any()) }
+    }
+
+    private fun buildCustomer(
+        id: Int? = null,
+        name: String = "customer name",
+        email: String = "${UUID.randomUUID()}@email.com",
+        password: String = "password",
+    ) = CustomerModel(
+            id = id,
+            name = name,
+            email = email,
+            password = password,
+            status = CustomerStatus.ATIVO,
+            roles = setOf(Role.CUSTOMER)
+        )
+
+
+}
